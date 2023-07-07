25eeb55835e3b72da8f2fd70e70e6ab813035f65
sandra ogbudinkpa <sandra.os325@gmail.com>
2023-07-07 18:56:34 -0400

    creates DOM modifying elements

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+// Get the main DOM node
+let mainNode = document.getElementById('main');
+
+// Remove the main DOM node
+mainNode.remove();
+
+// Create an h1 DOM node
+let newHeader = document.createElement('h1');
+
+// Set the id of the h1 DOM node
+newHeader.id = 'victory';
+
+// Set the text of the h1 DOM node
+newHeader.textContent = 'YOUR-NAME is the champion'; // Replace YOUR-NAME with your name
+
+// Append newHeader to the body
+document.body.appendChild(newHeader);
